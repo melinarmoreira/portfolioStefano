0a6eeb73ddf7fd9800bf5ea1538b832e0451bf65
efimero <martinperez9713@gmail.com>
2022-05-30 17:45:42 -0300

    fix navigation

--- src/components/Contacto/Contacto.js
+++ src/components/Contacto/Contacto.js
@@ -7,14 +7,15 @@ export const Contacto = () => {
         <div id='img-content'>
             <img src={imgContacto} alt="imagen de cartas" />
         </div>
+        <div id='formulario-content'></div>
         <form action="" id='formulario'>
-            <h4>CONTACTO</h4>
-            <h5>¡Escríbeme y te contestaré lo antes posible!</h5>
-            <input type="text" placeholder='Escribe tu nombre' />
-            <input type="email" placeholder='Escribe tu correo' />
-            <textarea name="" id="" cols="30" rows="10" placeholder='Escribe tu mensaje...' />
-            <input type="submit" value={'ENVIAR'} id="submit"/>
-        </form>
+          <h4>CONTACTO</h4>
+          <h5>¡Escríbeme y te contestaré lo antes posible!</h5>
+          <input type="text" placeholder='Escribe tu nombre' />
+          <input type="email" placeholder='Escribe tu correo' />
+          <textarea name="" id="" cols="30" rows="10" placeholder='Escribe tu mensaje...' />
+          <input type="submit" value={'ENVIAR'} id="submit"/>
+        </form>    
     </div>
   )
 }
