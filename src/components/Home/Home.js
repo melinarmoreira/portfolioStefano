9dae2b17df936edd2c2a2076e529fd0f522c4694
efimero <martinperez9713@gmail.com>
2022-06-15 16:55:43 -0300

    fix portfolio & responsive

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,10 +2,15 @@ import React from 'react'
 import './Home.scss'
 import { FaAngleDown } from "react-icons/fa";
 import stefano from "../../img/home/home.png"
+import vector from "../../img/home/vector.png"
 import { AiOutlineWhatsApp } from "react-icons/ai";
 // import { Link } from 'react-router-dom';
 
 export const Home = () => {
+
+  const enviarWsp = () =>{
+    window.location.assign("https://api.whatsapp.com/send?phone=5492914719897&text=Buenos%20dias%20Stefano%20estuve%20viendo%20tu%20portfolio...");
+  }
   const containerStyle = {
     backgroundImage:
       `url(${stefano})`,
@@ -15,6 +20,8 @@ export const Home = () => {
     <div id='home'>
         <div id='content-profile-welcome'>
           <div id='burbujas'>
+              <img className='vector vector-top'src={vector} />
+              <img className='vector vector-bottom'src={vector} />
               <div id='burbuja'>
                 <div id='burbuja-naranja' style={containerStyle}>
                   <div id='gradient'></div>
@@ -29,9 +36,9 @@ export const Home = () => {
           </div>
         </div>
         <div id='arrow'>
-          <div id='wsp'>
+          <a id='wsp' href="https://api.whatsapp.com/send?phone=5492914719897&text=Buenos%20dias%20Stefano%20estuve%20viendo%20tu%20portfolio..." target="_blank">
             <AiOutlineWhatsApp className='wsp-icon'/>
-          </div>
+          </a>
           <a href='#sobre-mi-content'>
             <FaAngleDown className='arrow-down' id='arrow-25'/>
             <FaAngleDown className='arrow-down' id='arrow-55'/>
--- src/components/Portfolio/WidgetPortfolio.js
+++ src/components/Portfolio/WidgetPortfolio.js
@@ -3,7 +3,7 @@ import "./Portfolio.scss";
 export const WidgetPortfolio = ({ info }) => {
    return (
       <div className="widget">
-         <a href={info.url}>
+         <a href={info.url} target="_blank">
             <img src={info.imagen} className="img-widget" alt={info.descripcion}></img>
             <div className="txt-widget">
             <p>{info.descripcion}</p>
